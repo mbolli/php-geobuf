734c5e51faebb2b7f12e185fd0af20920fbbfeb2
Michael Bolli <michael@bolli.us>
2022-12-19 09:44:26 +0100

    update php-cs-fixer ruleset

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -8,31 +8,19 @@ $config = new PhpCsFixer\Config();
 return $config
     ->setRiskyAllowed(true)
     ->setRules([
+        '@PhpCsFixer' => true,
+        '@PhpCsFixer:risky' => true,
         '@PHP80Migration:risky' => true,
-        '@PHP82Migration' => true,
         '@PSR12' => true,
         '@PSR12:risky' => true,
         '@Symfony' => true,
         '@Symfony:risky' => true,
-        'align_multiline_comment' => true,
-        'array_syntax' => true,
-        'braces' => ['position_after_control_structures'=>'same','position_after_functions_and_oop_constructs'=>'same'],
-        'combine_consecutive_issets' => true,
-        'combine_consecutive_unsets' => true,
+        'braces' => ['allow_single_line_closure' => true, 'position_after_functions_and_oop_constructs' => 'same'],
         'concat_space' => ['spacing' => 'one'],
         'declare_strict_types' => false,
-        'explicit_indirect_variable' => true,
-        'explicit_string_variable' => true,
-        'global_namespace_import' => true,
         'mb_str_functions' => true,
-        'no_useless_else' => true,
-        'no_useless_return' => true,
         'operator_linebreak' => true,
-        'phpdoc_no_empty_return' => true,
-        'phpdoc_types_order' => true,
-        'simple_to_complex_string_variable' => true,
-        'simplified_if_return' => true,
-        'yoda_style' => true,
+        'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
     ])
     ->setFinder(PhpCsFixer\Finder::create()
         ->exclude('GPBMetadata')
